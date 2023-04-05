b9a098febed542e236119730a2073cd30b48b5e5
Tom <thomasjhouf@gmail.com>
2023-04-05 05:57:13 -0400

    fleshed out some of the about.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import profile from '../images/profile.png'
+
+const styles = {
+  img: {
+    borderRadius: '45%',
+    width: '180px',
+    height: '180px',
+    display: 'block',
+    margin: '0 auto'
+  },
+
+  btnFloatingImg: {
+    width: '100%',
+    height: '100%',
+  }
+}
 
 export default function About() {
   return (
     <div>
-      <h1>About Tom</h1>
+      <h1>Tom Houf</h1>
+      <img alt="profile" src={profile} style={styles.img} />
       <p>
-      As an entry-level web developer, I have experience in handling HTML, CSS, and JavaScript, and have worked with both MySQL using Sequelize and MongoDB using Mongoose for database management. I also have a strong understanding of PWA and React. I am excited to continue learning and growing as a web developer and am eager to take on new challenges.
+        Tom Houf is a graduate of the Case Western Reserve full-stack web development bootcamp.
+      </p>
+      <p>
+        As an entry-level web developer, I have experience in handling HTML, CSS, and JavaScript, and have worked with both MySQL using Sequelize and MongoDB using Mongoose for database management. I also have a strong understanding of PWA and React. I am excited to continue learning and growing as a web developer and am eager to take on new challenges.
       </p>
     </div>
   );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,14 @@
-import React from 'react';
 import profile from '../images/profile.png'
+
+
 
 export default function Home() {
   return (
     <div>
       <h1>Tom Houf</h1>
-      <img alt="profile image" src={profile}/>
-      <h4>
+       <p>
 Tom Houf is a graduate of the Case Western Reserve full-stack web development bootcamp.
-      </h4>
+      </p>
     </div>
   );
 }
